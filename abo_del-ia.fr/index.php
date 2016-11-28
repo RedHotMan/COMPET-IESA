29eb23bc1cda2a5e7e22265163733485aa86145c
RedHotMan <ilyace.regaibi@gmail.com>
2016-11-28 18:12:35 +0100

    Add part service, Add social media part, Add CTA for the test, update CSS

--- abo_del-ia.fr/index.php
+++ abo_del-ia.fr/index.php
@@ -9,7 +9,7 @@
         <meta name="description" content="">
         <meta name="keywords" content="">
         <link rel="shortcut icon" type="image/png" href="media/logo/" />
-        <link rel="stylesheet" type="text/css" href="css/reset.css" />
+<!--        <link rel="stylesheet" type="text/css" href="css/reset.css" />-->
         <link rel="stylesheet" type="text/css" href="css/main.css" />
         <script type="text/javascript" src="js/jquery.min.js"></script>
         <script type="text/javascript" src="js/main.js"></script>
@@ -29,12 +29,46 @@
             </div>
         </section>
         
-        <main>
+        <section id="anchor">
+            <h1> TITLE OF THIS SECTION</h1>
+            <div class="row">
+                <div class="col-1-2">
+                    <h2> Logo service + TITLE SERVICE</h2>
+                    <p>Fuerit toto in consulatu sine provincia, cui fuerit, antequam designatus est, decreta provincia. Sortietur an non? Nam et non sortiri absurdum est, et, quod sortitus sis, non habere. Proficiscetur paludatus? Quo? Quo pervenire ante certam diem non licebit. ianuario, Februario, provinciam non habebit; Kalendis ei denique Martiis nascetur repente provincia.</p>
+                </div>
+                
+                <div class="col-1-2">
+                    <h2> Logo service + TITLE SERVICE</h2>
+                    <p>Fuerit toto in consulatu sine provincia, cui fuerit, antequam designatus est, decreta provincia. Sortietur an non? Nam et non sortiri absurdum est, et, quod sortitus sis, non habere. Proficiscetur paludatus? Quo? Quo pervenire ante certam diem non licebit. ianuario, Februario, provinciam non habebit; Kalendis ei denique Martiis nascetur repente provincia.</p>
+                </div>
+            </div>
             
-        </main>
+            <div class="row">
+                <div class="col-1-2">
+                    <h2> Logo service + TITLE SERVICE</h2>
+                    <p>Fuerit toto in consulatu sine provincia, cui fuerit, antequam designatus est, decreta provincia. Sortietur an non? Nam et non sortiri absurdum est, et, quod sortitus sis, non habere. Proficiscetur paludatus? Quo? Quo pervenire ante certam diem non licebit. ianuario, Februario, provinciam non habebit; Kalendis ei denique Martiis nascetur repente provincia.</p>
+                </div>
+                
+                <div class="col-1-2">
+                    <h2> Logo service + TITLE SERVICE</h2>
+                    <p>Fuerit toto in consulatu sine provincia, cui fuerit, antequam designatus est, decreta provincia. Sortietur an non? Nam et non sortiri absurdum est, et, quod sortitus sis, non habere. Proficiscetur paludatus? Quo? Quo pervenire ante certam diem non licebit. ianuario, Februario, provinciam non habebit; Kalendis ei denique Martiis nascetur repente provincia.</p>
+                </div>
+            </div>
+            <button class="cta_test">Tester Delia</button>
+        </section>
 
-        <footer>
-            <div>
+       <footer>
+            <div id="reseaux">
+                <div>
+                    <ul>
+                        <li><img src="media/facebook.png"></li>
+                        <li><img src="media/twitter.png"></li>
+                        <li><img src="media/pinterest.png"></li>
+                        <li><img src="media/linkedin.png"></li>
+                    </ul>
+                </div>
+            </div>
+            <div id="copyright">
                 <p>Site développé par SoComman- 2016</p>
             </div>
         </footer>

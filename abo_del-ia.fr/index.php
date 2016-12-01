9ed7b06c8f023135a3838300a072c79a19f2d3b0
abo8 <8bo.alexandre@gmail.com>
2016-12-01 11:57:06 +0100

    Update index.php
    Ajout de la grille, de la home, des liens de footer et adaptation au css

--- abo_del-ia.fr/index.php
+++ abo_del-ia.fr/index.php
@@ -8,8 +8,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="description" content="">
         <meta name="keywords" content="">
-        <link rel="shortcut icon" type="image/png" href="media/logo/" />
-<!--        <link rel="stylesheet" type="text/css" href="css/reset.css" />-->
+        <link rel="shortcut icon" type="image/png" href="media/logos/favicon.png" />
+        <link rel="stylesheet" type="text/css" href="css/reset.css" />
         <link rel="stylesheet" type="text/css" href="css/main.css" />
         <script type="text/javascript" src="js/jquery.min.js"></script>
         <script type="text/javascript" src="js/main.js"></script>
@@ -20,14 +20,21 @@
 
         <div id="loader"></div>
         
-        <section class="first">
-            <div>
-                <img src="media/logo.png" class="fade">
-                <h1 class="effect">DELIA</h1>
-
-                <a class="cta fade"><span>Explorer</span></a></div>
+        <main class="first">
+            <div class="fadein">
+                <img src="media/bg/1.jpg">
+                <img src="media/bg/2.jpg">
+                <img src="media/bg/3.jpg">
+                <img src="media/bg/4.jpg">
+                <img src="media/bg/5.jpg">
             </div>
-        </section>
+            <div id="pres">
+                <img src="media/logos/logo.png" class="fade">
+                <h1 class="effect">DELIA</h1>
+                <p class="effect">L'intelligence artificielle qui vous facilite la vie</p><br>
+                <a class="cta intro fade"><span>Explorer</span></a></div>
+            </div>
+        </main>
         
         <section id="anchor">
             <h1> TITLE OF THIS SECTION</h1>
@@ -41,9 +48,7 @@
                     <h2> Logo service + TITLE SERVICE</h2>
                     <p>Fuerit toto in consulatu sine provincia, cui fuerit, antequam designatus est, decreta provincia. Sortietur an non? Nam et non sortiri absurdum est, et, quod sortitus sis, non habere. Proficiscetur paludatus? Quo? Quo pervenire ante certam diem non licebit. ianuario, Februario, provinciam non habebit; Kalendis ei denique Martiis nascetur repente provincia.</p>
                 </div>
-            </div>
-            
-            <div class="row">
+
                 <div class="col-1-2">
                     <h2> Logo service + TITLE SERVICE</h2>
                     <p>Fuerit toto in consulatu sine provincia, cui fuerit, antequam designatus est, decreta provincia. Sortietur an non? Nam et non sortiri absurdum est, et, quod sortitus sis, non habere. Proficiscetur paludatus? Quo? Quo pervenire ante certam diem non licebit. ianuario, Februario, provinciam non habebit; Kalendis ei denique Martiis nascetur repente provincia.</p>
@@ -57,19 +62,14 @@
             <button class="cta_test">Tester Delia</button>
         </section>
 
-       <footer>
-            <div id="reseaux">
-                <div>
-                    <ul>
-                        <li><img src="media/facebook.png"></li>
-                        <li><img src="media/twitter.png"></li>
-                        <li><img src="media/pinterest.png"></li>
-                        <li><img src="media/linkedin.png"></li>
-                    </ul>
-                </div>
-            </div>
-            <div id="copyright">
-                <p>Site développé par SoComman- 2016</p>
+        <footer>
+            <div class="center">
+                <p>Site développé par SoComman - 2016 - <a href="#">Contact</a></p>
+                <ul>
+                    <li><a href="https://www.facebook.com/askdelia/" class="logo" target="_blank" id="facebook"></a></li>
+                    <li><a href="https://twitter.com/DeliaCoach/" class="logo" target="_blank" id="twitter"></a></li>
+                    <li><a href="https://fr.pinterest.com/brosocomman/" class="logo" target="_blank" id="pinterest"></a></li>
+                </ul>
             </div>
         </footer>
 

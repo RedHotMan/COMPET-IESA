fff7067d351536b7edc0b6bbdbe50b3bce07f69f
abo8 <8bo.alexandre@gmail.com>
2016-12-02 18:11:24 +0100

    Update test-ia.php

--- abo_del-ia.fr/test-ia.php
+++ abo_del-ia.fr/test-ia.php
@@ -5,7 +5,7 @@
     <head>
         <title>Testez DELIA, l'IA de demain</title>
         <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="viewport" content="width=device-width; height=device-height; user-scalable=0; initial-scale=1.0; maximum-scale=1.0;">
         <link rel="shortcut icon" type="image/png" href="media/logos/favicon.png" />
         <link rel="stylesheet" type="text/css" href="css/reset.css" />
         <link rel="stylesheet" type="text/css" href="css/test-ia.css" />
@@ -17,8 +17,11 @@
     <body id="test-ia">
 
         <main class="first">
+            <audio autoplay>
+                <source src="media/mp3/test.mp3" type="audio/mpeg">
+            </audio>
             <div id="pres">
-                <img src="media/logos/logo.png" class="fade">
+                <img src="media/logos/logo2.png" class="fade">
                 <h1 class="fade">TESTEZ DELIA</h1>
                 <p class="fade">Un aperçu des fonctionnalités</p><br>
                 <a class="cta intro fade" id="test-begin"><span>Commencer</span></a>
@@ -29,10 +32,11 @@
             <div class="content">
                 <div id="quest1" class="quest">
                     <label>Que recherchez-vous ?</label>
-                    <select>
-                        <option value="volvo">Volvo</option>
-                        <option value="saab">Saab</option>
-                        <option value="opel">Opel</option>
+                    <select id="select-rech">
+                        <option value="null">Choisissez votre activité</option>
+                        <option value="volvo">1</option>
+                        <option value="saab">2</option>
+                        <option value="opel">3</option>
                     </select>
                     <input type='button' name="valid-rech" id="but-rech" value="OK">
                 </div>
@@ -47,8 +51,15 @@
                     <input type='button' name="valid-time" id="but-time" value="OK">
                 </div>
             </div>
-            <span id="loader" ></span>
         </section>
+
+        <span id="loader" ></span>
+
+        <section id="rep" class="center hello">
+            <p class="p1">Alors ça t'a plu batard ?</p>
+            <p class="p2">T' as cru vraiment qu'on allait développer une IA pour toi ?</p>
+        </section>
+
 
     </body>
 

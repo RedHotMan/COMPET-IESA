49c9566e9a8fb076528db65dac5974411d4366d6
abo8 <8bo.alexandre@gmail.com>
2016-12-06 19:59:16 +0100

    Ajout de #message

--- abo_del-ia.fr/test-ia.php
+++ abo_del-ia.fr/test-ia.php
@@ -10,15 +10,15 @@
         <link rel="stylesheet" type="text/css" href="css/reset.css" />
         <link rel="stylesheet" type="text/css" href="css/test-ia.css" />
         <script type="text/javascript" src="js/jquery.min.js"></script>
-        <script type="text/javascript" src="js/main.js"></script>
+        <script type="text/javascript" src="js/test-ia.js"></script>
     </head>
     
     
     <body id="test-ia">
 
         <span id="loader"></span>
+        <span id="attention"></span>
         <span id="warn"></span>
-        <span id="modal-video"><iframe src="https://www.youtube.com/embed/ch7gquxwohA" frameborder="0" allowfullscreen></iframe></span>
 
         <main class="first">
             <audio id="hi">
@@ -56,7 +56,7 @@
                             <option value="null" selected></option>
                             <option value="1">Regarder une série/un film</option>
                             <option value="2">S'instruire/Tutoriels</option>
-                            <option value="3">Joueur à un jeu</option>
+                            <option value="3">Jouer à un jeu vidéo</option>
                             <option value="4">Se détendre</option>
                             <option value="5">Se défouler (coaching sport)</option>
                         </select>
@@ -73,31 +73,66 @@
                     </div>
                     <div class=" reps">
                         <p>Voici ce que je peux vous proposer :</p>
-                        <ul class="props props-1">
-                            <li>Réponse 1-1</li>
-                            <li>Réponse 1-2</li>
-                            <li>Réponse 1-3</li>
-                        </ul>
-                        <ul class="props props-2">
-                            <li>Réponse 1-1</li>
-                            <li>Réponse 1-2</li>
-                            <li>Réponse 1-3</li>
-                        </ul>
-                        <ul class="props props-3">
-                            <li>Réponse 1-1</li>
-                            <li>Réponse 1-2</li>
-                            <li>Réponse 1-3</li>
-                        </ul>
-                        <ul class="props props-4">
-                            <li>Réponse 1-1</li>
-                            <li>Réponse 1-2</li>
-                            <li>Réponse 1-3</li>
-                        </ul>
-                        <ul class="props props-5">
-                            <li>Réponse 1-1</li>
-                            <li>Réponse 1-2</li>
-                            <li>Réponse 1-3</li>
-                        </ul>
+                        <div class="props-1">
+                            <ul class="props l1">
+                                <li><img src="media/reps/les-temps-modernes.jpg"/><p>Les Temps Modernes</p></li>
+                                <li><img src="media/reps/game-of-thrones.jpg"/><p>Game Of Thrones</p></li>
+                                <li><img src="media/reps/remi.jpg"/><p>Remi Sans Famille</p></li>
+                            </ul>
+                            <ul class="props l2">
+                                <li><img src="media/reps/black-mirror.jpg"/><p>Black Mirror</p></li>
+                                <li><img src="media/reps/SOC-NWA.jpg"/><p>Straight Outta Compton</p></li>
+                                <li><img src="media/reps/the-hateful-eight.jpg"/><p>Les 8 Salopards</p></li>
+                            </ul>
+                        </div>
+                        <div class="props-2">
+                            <ul class="props l1">
+                                <li><img src="media/reps/yt.jpg"/><p>Tutoriel Youtube</p></li>
+                                <li><img src="media/reps/dcdl.jpg"/><p>Regarder Des Chiffres et Des Lettres</p></li>
+                                <li><img src="media/reps/drague.jpg"/><p>La drague pour les nuls</p></li>
+                            </ul>
+                            <ul class="props l2">
+                                <li><img src="media/reps/arte.jpg"/><p>Documentaire sur Arte</p></li>
+                                <li><img src="media/reps/trivial-pursuit.jpg"/><p>Trivial Pursuit</p></li>
+                                <li><img src="media/reps/ecole-primaire.jpg"/><p>L'école prmaire</p></li>
+                            </ul>
+                        </div>
+                        <div class="props-3">
+                            <ul class="props l1">
+                                <li><img src="media/reps/the-witcher-3.jpg"/><p>The Witcher 3</p></li>
+                                <li><img src="media/reps/watch-dogs-2.jpg"/><p>Watch Dogs 2</p></li>
+                                <li><img src="media/reps/alone.jpg"/><p>Alone in the Dark</p></li>
+                            </ul>
+                            <ul class="props l2">
+                                <li><img src="media/reps/wow.jpg"/><p>World Of Warcraft</p></li>
+                                <li><img src="media/reps/fifa.jpg"/><p>FIFA 2017</p></li>
+                                <li><img src="media/reps/goat-simulator.jpg"/><p>Goat Simulator</p></li>
+                            </ul>
+                        </div>
+                        <div class="props-4">
+                            <ul class="props l1">
+                                <li><img src="media/reps/yoga.jpg"/><p>Yoga</p></li>
+                                <li><img src="media/reps/"/><p></p></li>
+                                <li><img src="media/reps/fille-de-ta-region.jpg"/><p>Contacter une fille près de chez toi</p></li>
+                            </ul>
+                            <ul class="props l2">
+                                <li><img src="media/reps/yoga.jpg"/><p>Yoga</p></li>
+                                <li><img src="media/reps/amour.jpg"/><p>Sport de chambre</p></li>
+                                <li><img src="media/reps/monopoly.jpg"/><p>MONOPOLY</p></li>
+                            </ul>
+                        </div>
+                        <div class="props-5">
+                            <ul class="props l1">
+                                <li><img src="media/reps/boxe.jpg"/><p>Boxe</p></li>
+                                <li><img src="media/reps/"/><p></p></li>
+                                <li><img src="media/reps/"/><p>Réponse 1-3</p></li>
+                            </ul>
+                            <ul class="props l2">
+                                <li><img src="media/reps/bar-motard.jpg"/><p>Bar de Motard</p></li>
+                                <li><img src="media/reps/boxe.jpg"/><p>Boxe</p></li>
+                                <li><img src="media/reps/amour.jpg"/><p></p></li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
 
@@ -117,30 +152,38 @@
                     <div id="q-2-3" class="q-3 quest hello">
                         <label>Où ?</label>
                         <input type="text" name="location" id="text-loation" placeholder="Ex : Paris, Strasbourg, Perpignan...  "/>
-                        <input type='button' name="rep-3" class="but-rep3" value="OK">
+                        <input type='button' name="rep-3" class="but-rep3 inbut" value="OK">
                     </div>
                     <div class=" reps">
                         <p>Voici ce que je peux vous proposer :</p>
-                        <ul class="props props-1">
-                            <li>Réponse 2-1</li>
-                            <li>Réponse 2-2</li>
-                            <li>Réponse 2-3</li>
-                        </ul>
-                        <ul class="props props-2">
-                            <li>Réponse 2-1</li>
-                            <li>Réponse 2-2</li>
-                            <li>Réponse 2-3</li>
-                        </ul>
-                        <ul class="props props-3">
-                            <li>Réponse 2-1</li>
-                            <li>Réponse 2-2</li>
-                            <li>Réponse 2-3</li>
-                        </ul>
-                        <ul class="props props-4">
-                            <li>Réponse 2-1</li>
-                            <li>Réponse 2-2</li>
-                            <li>Réponse 2-3</li>
-                        </ul>
+                        <div class="props-1">
+                            <ul class="props l1">
+                                <li><img src="media/reps/blueelephant.jpg"/><p>Blue Elephant</p></li>
+                                <li><img src="media/reps/skylinebar.jpg"/><p>Le Skyline Bar & Lounge</p></li>
+                                <li><img src="media/reps/barasieste.jpg"/><p>Bar à sieste</p></li>
+                            </ul>
+                        </div>
+                        <div class="props-2">
+                            <ul class="props l1">
+                                <li><img src="media/reps/grandrex.jpg"/><p>Le Grand Rex</p></li>
+                                <li><img src="media/reps/theatre-du-chatelet.jpg"/><p>Théâtre du Chatêlet</p></li>
+                                <li><img src="media/reps/moulin-rouge.jpg"/><p>Le Moulin Rouge</p></li>
+                            </ul>
+                        </div>
+                        <div class="props-3">
+                            <ul class="props l1">
+                                <li><img src="media/reps/casino.jpg"/><p>Casino Enghien-Les-Bains</p></li>
+                                <li><img src="media/reps/jardin.jpg"/><p>Jardin du panthéon bouddhique</p></li>
+                                <li><img src="media/reps/cimetiere.jpg"/><p>Cimetière du Père Lachaise</p></li>
+                            </ul>
+                        </div>
+                        <div class="props-4">
+                            <ul class="props l1">
+                                <li><img src="media/reps/patinoire.jpg"/><p>Patinoire du Grand Palais</p></li>
+                                <li><img src="media/reps/bowling.jpg"/><p>Le Bowling Foch</p></li>
+                                <li><img src="media/reps/curling.jpg"/><p>Curling Club Viry-Chatillon</p></li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
 
@@ -170,31 +213,91 @@
                     </div>
                     <div class=" reps">
                         <p>Voici ce que je peux vous proposer :</p>
-                        <ul class="props props-1">
-                            <li>Réponse 3-1</li>
-                            <li>Réponse 3-1</li>
-                            <li>Réponse 3-1</li>
-                        </ul>
-                        <ul class="props props-2">
-                            <li>Réponse 3-2</li>
-                            <li>Réponse 3-2</li>
-                            <li>Réponse 3-2</li>
-                        </ul>
-                        <ul class="props props-3">
-                            <li>Réponse 3-3</li>
-                            <li>Réponse 3-3</li>
-                            <li>Réponse 3-3</li>
-                        </ul>
-                        <ul class="props props-4">
-                            <li>Réponse 3-4</li>
-                            <li>Réponse 3-4</li>
-                            <li>Réponse 3-4</li>
-                        </ul>
-                        <ul class="props props-5">
-                            <li>Réponse 3-5</li>
-                            <li>Réponse 3-5</li>
-                            <li>Réponse 3-5</li>
-                        </ul>
+                        <div class="props-1">
+                            <ul class="props l1">
+                                <li><img src="media/reps/coffret-parfum.jpg"/><p>Coffret Parfum</p></li>
+                                <li><img src="media/reps/livre-régime.jpg"/><p>Livre sur le régime</p></li>
+                                <li><img src="media/reps/spray-cheveux.jpg"/><p>Spray anti-chute de cheveux</p></li>
+                            </ul>
+                            <ul class="props l2">
+                                <li><img src="media/reps/maquillage.jpg"/><p>Coffret Cosmétiques</p></li>
+                                <li><img src="media/reps/coussin.jpg"/><p>Boyfriend Arm Luxury Support Pillow</p></li>
+                                <li><img src="media/reps/pesepersonne.jpg"/><p>Pèse-personne</p></li>
+                            </ul>
+                            <ul class="props l3">
+                                <li><img src="media/reps/coffret.jpg"/><p>Coffret thalasso / spa</p></li>
+                                <li><img src="media/reps/livre-bébé.jpg"/><p>Livre sur la conception des bébés</p></li>
+                                <li><img src="media/reps/kit.jpg"/><p>Des menottes/kit chatouille-moi</p></li>
+                            </ul>
+                        </div>
+                        <div class="props-2">
+                            <ul class="props l1">
+                                <li><img src="media/reps/coffret-parfum.jpg"/><p>Coffret Parfum</p></li>
+                                <li><img src="media/reps/livre-régime.jpg"/><p>Livre sur le régime</p></li>
+                                <li><img src="media/reps/spray-cheveux.jpg"/><p>Spray anti-chute de cheveux</p></li>
+                            </ul>
+                            <ul class="props l2">
+                                <li><img src="media/reps/maquillage.jpg"/><p>Coffret Cosmétiques</p></li>
+                                <li><img src="media/reps/coussin.jpg"/><p>Boyfriend Arm Luxury Support Pillow</p></li>
+                                <li><img src="media/reps/pesepersonne.jpg"/><p>Pèse-personne</p></li>
+                            </ul>
+                            <ul class="props l3">
+                                <li><img src="media/reps/coffret.jpg"/><p>Coffret thalasso / spa</p></li>
+                                <li><img src="media/reps/livre-bébé.jpg"/><p>Livre sur la conception des bébés</p></li>
+                                <li><img src="media/reps/kit.jpg"/><p>Des menottes/kit chatouille-moi</p></li>
+                            </ul>
+                        </div>
+                        <div class="props-3">
+                            <ul class="props l1">
+                                <li><img src="media/reps/doudou-garcon.jpg"/><p>Jouet educatif/doudou</p></li>
+                                <li><img src="media/reps/tetine-garcon.jpg"/><p>Tétine drôle</p></li>
+                                <li><img src="media/reps/body.jpg"/><p>Body bébé fragile</p></li>
+                            </ul>
+                            <ul class="props l2">
+                                <li><img src="media/reps/doudou.jpg"/><p>Doudou fille</p></li>
+                                <li><img src="media/reps/tetine-fille.JPG"/><p>Tétine drole</p></li>
+                                <li><img src="media/reps/accroche.jpg"/><p>Accroche-bébé</p></li>
+                            </ul>
+                            <ul class="props  l3">
+                                <li><img src="media/reps/doudou.jpg"/><p>Doudou pour le nouveau-né</p></li>
+                                <li><img src="media/reps/livre-parent.jpg"/><p>Livre être parent aujourd'hui</p></li>
+                                <li><img src="media/reps/accroche.jpg"/><p>Accroche-bébé</p></li>
+                            </ul>
+                        </div>
+                        <div class="props-4">
+                            <ul class="props l1">
+                                <li><img src="media/reps/coffret-parfum.jpg"/><p>Coffret Parfum</p></li>
+                                <li><img src="media/reps/livre-régime.jpg"/><p>Livre sur le régime</p></li>
+                                <li><img src="media/reps/spray-cheveux.jpg"/><p>Spray anti-chute de cheveux</p></li>
+                            </ul>
+                            <ul class="props l2">
+                                <li><img src="media/reps/hotel.jpg"/><p>Sejour au 7ème ciel : Hotel chambre galaxy</p></li>
+                                <li><img src="media/reps/anti-age.jpg"/><p>Gampe de soin anti-age</p></li>
+                                <li><img src="media/reps/panoplie-sextoy.jpg"/><p>Sextoy (panoplie fifty shades of grades)</p></li>
+                            </ul>
+                            <ul class="props l3">
+                                <li><img src="media/reps/coffret.jpg"/><p>Coffret thalasso / spa</p></li>
+                                <li><img src="media/reps/livre-bébé.jpg"/><p>Livre sur la conception des bébés</p></li>
+                                <li><img src="media/reps/kit.jpg"/><p>Des menottes/kit chatouille-moi</p></li>
+                            </ul>
+                        </div>
+                        <div class="props-5">
+                            <ul class="props l1">
+                                <li><img src="media/reps/ferrari.jpg"/><p>Stage en ferrari</p></li>
+                                <li><img src="media/reps/livre-connard.jpg"/><p>Livre : comment devenir un connard</p></li>
+                                <li><img src="media/reps/gel-homme.jpg"/><p>Gel abdominal</p></li>
+                            </ul>
+                            <ul class="props l2">
+                                <li><img src="media/reps/bijoux.jpg"/><p>Bjoux</p></li>
+                                <li><img src="media/reps/cafetiere.jpg"/><p>Cafetière</p></li>
+                                <li><img src="media/reps/anticellulite.jpg"/><p>Crème cellulite/fermeté pour le corps</p></li>
+                            </ul>
+                            <ul class="props l3">
+                                <li><img src="media/reps/lovebox.jpg"/><p>Boîte love box : mille et une nuits</p></li>
+                                <li><img src="media/reps/portecles.jpg"/><p>Porte-clés gravé</p></li>
+                                <li><img src="media/reps/pochettecadeau.jpg"/><p>Pochette cadeau : instrument de plaisir</p></li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -203,8 +306,14 @@
         <section id="message" class="center hello">
             <p class="p1">Un instant...</p>
             <p class="p2">Avez-vous vu les différents problèmes de notre IA ?</p>
-            <p class="p3">C'est bien normal...<br> <span>DELIA est en train de dégénérer.</span></p>
+            <p class="p3">C'est bien normal...<br><span>DELIA est en train de dégénérer.</span></p>
+            <p class="p4"><a class="cta" id="if-cta">Son secret</a></p>
+            <p class="p5">Ne soyez plus dominés par vos smartphones.</p>
+            <p class="p6">Reprenez le pouvoir.</p>
         </section>
+
+        <div id="modal-video"><iframe src="https://www.youtube.com/embed/ch7gquxwohA" id="video" frameborder="0" allowfullscreen></iframe><span></span></div>
+
 
     </body>
 

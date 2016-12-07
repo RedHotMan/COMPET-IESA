2ce601c227f292d0a98d39a2fc9ba3c68698c37a
abo8 <8bo.alexandre@gmail.com>
2016-12-07 12:01:49 +0100

    Update test-ia.js

--- abo_del-ia.fr/js/test-ia.js
+++ abo_del-ia.fr/js/test-ia.js
@@ -31,7 +31,7 @@ $(window).load(function(){
 $(document).ready(function(){
 
     $('#test-begin').click(function(){
-        $('#test-ia').css('background','url(media/bg/test-ia/2.jpg) center no-repeat').css('background-size','cover');
+        $('#test-ia').css('background','url(media/bg/2.jpg) center no-repeat').css('background-size','cover');
         $('main').addClass('bye-bye');
         $('#quests').removeClass('hello').addClass('present');
         $('#loader').fadeIn(500).delay(1000).fadeOut(500);
@@ -41,7 +41,7 @@ $(document).ready(function(){
     $( "#slct-1" ).change(function() {
         $("#slct-1 option[value='1']:selected").each(function () {
             $('.but-rep1').click(function(){
-                $('#test-ia').css('background','url(media/bg/test-ia/3.jpg) center no-repeat').css('background-size','cover');
+                $('#test-ia').css('background','url(media/bg/3.jpg) center no-repeat').css('background-size','cover');
                 $('#q-1-2, #q-1').removeClass('hello');
                 $('#q-2, #q-3').remove();
                 $("#slct-1").attr('disabled','disabled');
@@ -50,7 +50,7 @@ $(document).ready(function(){
         });
         $("#slct-1 option[value='2']:selected").each(function () {
             $('.but-rep1').click(function(){
-                $('#test-ia').css('background','url(media/bg/test-ia/3.jpg) center no-repeat').css('background-size','cover');
+                $('#test-ia').css('background','url(media/bg/3.jpg) center no-repeat').css('background-size','cover');
                 $('#q-2-2,#q-2').removeClass('hello');
                 $('#q-1, #q-3').remove();
                 $("#slct-1").attr('disabled','disabled');
@@ -59,7 +59,7 @@ $(document).ready(function(){
         });
         $("#slct-1 option[value='3']:selected").each(function () {
             $('.but-rep1').click(function(){
-                $('#test-ia').css('background','url(media/bg/test-ia/3.jpg) center no-repeat').css('background-size','cover');
+                $('#test-ia').css('background','url(media/bg/3.jpg) center no-repeat').css('background-size','cover');
                 $('#q-3-2, #q-3').removeClass('hello');
                 $('#q-1, #q-2').remove();
                 $("#slct-1").attr('disabled','disabled');
@@ -72,7 +72,7 @@ $(document).ready(function(){
     $( ".q-2 select" ).change(function() {
         $(".q-2 select option[value='1']:selected").each(function () {
             $('.but-rep2').click(function(){
-                $('#test-ia').css('background','url(media/bg/test-ia/4.jpg) center no-repeat').css('background-size','cover');
+                $('#test-ia').css('background','url(media/bg/4.jpg) center no-repeat').css('background-size','cover');
                 $('.props-2, .props-3, .props-4, .props-5').remove();
                 $('.q-3').removeClass('hello');
                 $(".q-2 select").attr('disabled','disabled');
@@ -81,7 +81,7 @@ $(document).ready(function(){
         });
         $(".q-2 select option[value='2']:selected").each(function () {
             $('.but-rep2').click(function(){
-                $('#test-ia').css('background','url(media/bg/test-ia/4.jpg) center no-repeat').css('background-size','cover');
+                $('#test-ia').css('background','url(media/bg/4.jpg) center no-repeat').css('background-size','cover');
                 $('.props-1, .props-3, .props-4, .props-5').remove();
                 $('.q-3').removeClass('hello');
                 $(".q-2 select").attr('disabled','disabled');
@@ -90,7 +90,7 @@ $(document).ready(function(){
         });
         $(".q-2 select option[value='3']:selected").each(function () {
             $('.but-rep2').click(function(){
-                $('#test-ia').css('background','url(media/bg/test-ia/4.jpg) center no-repeat').css('background-size','cover');
+                $('#test-ia').css('background','url(media/bg/4.jpg) center no-repeat').css('background-size','cover');
                 $('.props-1, .props-2, .props-4, .props-5').remove();
                 $('.q-3').removeClass('hello');
                 $(".q-2 select").attr('disabled','disabled');
@@ -99,7 +99,7 @@ $(document).ready(function(){
         });
         $(".q-2 select option[value='4']:selected").each(function () {
             $('.but-rep2').click(function(){
-                $('#test-ia').css('background','url(media/bg/test-ia/4.jpg) center no-repeat').css('background-size','cover');
+                $('#test-ia').css('background','url(media/bg/4.jpg) center no-repeat').css('background-size','cover');
                 $('.props-1, .props-2, .props-3, .props-5').remove();
                 $('.q-3').removeClass('hello');
                 $(".q-2 select").attr('disabled','disabled');
@@ -108,7 +108,7 @@ $(document).ready(function(){
         });
         $(".q-2 select option[value='5']:selected").each(function () {
             $('.but-rep2').click(function(){
-                $('#test-ia').css('background','url(media/bg/test-ia/4.jpg) center no-repeat').css('background-size','cover');
+                $('#test-ia').css('background','url(media/bg/4.jpg) center no-repeat').css('background-size','cover');
                 $('.props-1, .props-2, .props-3, .props-4').remove();
                 $('.q-3').removeClass('hello');
                 $(".q-2 select").attr('disabled','disabled');
@@ -126,7 +126,7 @@ $(document).ready(function(){
         });
         $(".q-3 select option[value='1']:selected").each(function () {
             $('.but-rep3').click(function() {
-                $('#test-ia').delay(2000).delay(1500).css('background','url(media/bg/test-ia/5.jpg) center no-repeat').css('background-size','cover');
+                $('#test-ia').delay(2000).delay(1500).css('background','url(media/bg/5.jpg) center no-repeat').css('background-size','cover');
                 $('#test-ia .quest, #test-ia .intro-qn ').delay(1000).addClass('bye-bye').delay(1000).fadeOut();
                 $('.l2, .l3, .l4, .l5').remove();
                 $('#loader').fadeIn(500).delay(3000).fadeOut(500);
@@ -135,7 +135,7 @@ $(document).ready(function(){
         });
         $(".q-3 select option[value='2']:selected").each(function () {
             $('.but-rep3').click(function() {
-                $('#test-ia').delay(2000).delay(1500).css('background','url(media/bg/test-ia/5.jpg) center no-repeat').css('background-size','cover');
+                $('#test-ia').delay(2000).delay(1500).css('background','url(media/bg/5.jpg) center no-repeat').css('background-size','cover');
                 $('#test-ia .quest, #test-ia .intro-qn ').delay(1000).addClass('bye-bye').delay(1000).fadeOut();
                 $('.l1, .l3, .l4, .l5').remove();
                 $('#loader').fadeIn(500).delay(3000).fadeOut(500);
@@ -144,7 +144,7 @@ $(document).ready(function(){
         });
         $(".q-3 select option[value='3']:selected").each(function () {
             $('.but-rep3').click(function() {
-                $('#test-ia').delay(2000).delay(1500).css('background','url(media/bg/test-ia/5.jpg) center no-repeat').css('background-size','cover');
+                $('#test-ia').delay(2000).delay(1500).css('background','url(media/bg/5.jpg) center no-repeat').css('background-size','cover');
                 $('#test-ia .quest, #test-ia .intro-qn ').delay(1000).addClass('bye-bye').delay(1000).fadeOut();
                 $('.l1, .l2, .l4, .l5').remove();
                 $('#loader').fadeIn(500).delay(3000).fadeOut(500);
@@ -153,7 +153,7 @@ $(document).ready(function(){
         });
         $(".q-3 select option[value='4']:selected").each(function () {
             $('.but-rep3').click(function() {
-                $('#test-ia').delay(2000).delay(1500).css('background','url(media/bg/test-ia/5.jpg) center no-repeat').css('background-size','cover');
+                $('#test-ia').delay(2000).delay(1500).css('background','url(media/bg/5.jpg) center no-repeat').css('background-size','cover');
                 $('#test-ia .quest, #test-ia .intro-qn ').delay(1000).addClass('bye-bye').delay(1000).fadeOut();
                 $('.l1, .l2, .l3, .l5').remove();
                 $('#loader').fadeIn(500).delay(3000).fadeOut(500);
@@ -162,7 +162,7 @@ $(document).ready(function(){
         });
         $(".q-3 select option[value='5']:selected").each(function () {
             $('.but-rep3').click(function() {
-                $('#test-ia').delay(2000).delay(1500).css('background','url(media/bg/test-ia/5.jpg) center no-repeat').css('background-size','cover');
+                $('#test-ia').delay(2000).delay(1500).css('background','url(media/bg/5.jpg) center no-repeat').css('background-size','cover');
                 $('#test-ia .quest, #test-ia .intro-qn ').delay(1000).addClass('bye-bye').delay(1000).fadeOut();
                 $('.l1, .l2, .l3, .l4').remove();
                 $('#loader').fadeIn(500).delay(3000).fadeOut(500);
@@ -175,23 +175,22 @@ $(document).ready(function(){
     $('.but-rep3.inbut').click(function() {
         if ($('#text-loation').val() == '') {
             alert('Remplissez le champ texte.');
-            console.log('rep1')
         } else {
-            $('#test-ia').delay(2000).delay(1500).css('background','url(media/bg/test-ia/5.jpg) center no-repeat').css('background-size','cover');
-            $('#test-ia .quest, #test-ia .intro-qn ').delay(1000).addClass('bye-bye').delay(1000).fadeOut();
+            $('#test-ia').delay(2000).delay(1500).css('background','url(media/bg/5.jpg) center no-repeat').css('background-size','cover');
+            $('#test-ia .quest, #test-ia .intro-qn ').delay(2000).addClass('bye-bye');
             $('#loader').fadeIn(500).delay(3000).fadeOut(500);
             $('.reps').delay(3000).fadeIn(500);
-            console.log('rep2')
         }
     });
 
     /******* Montrer message *******/
     $('.props li').click(function() {
-        $('#test-ia').css('background','url(media/bg/test-ia/2.jpg) center no-repeat').css('background-size','cover');
+        $('#test-ia').css('background','url(media/bg/2.jpg) center no-repeat').css('background-size','cover');
         $('#quests').addClass('bye-bye');
         $('#message').removeClass('hello').addClass('present');
         $('#loader').fadeIn(500).delay(1000).fadeOut(500);
         $('.p1').delay(2000).fadeIn(500).delay(2000).fadeOut(500);
+        $('#test-ia').delay(4000).css('background','url(media/bg/6.jpg) center no-repeat').css('background-size','cover');
         $('.p2').delay(5000).fadeIn(500).delay(4000).fadeOut(500);
         $('.p3').delay(10000).fadeIn(500).delay(5000).fadeOut(500);
         $('.p4').delay(16000).fadeIn(500);
@@ -205,10 +204,12 @@ $(document).ready(function(){
     });
 
     /******* End *******/
-    $('#modal-video span').click(function(ev) {
-        $('#modal-video').remove(500);
-        $('.p4').delay(16000).fadeOut(500);
+    $('#modal-video span').click(function() {
+        $('#modal-video').fadeOut(500).delay(1000).remove();
+        $('.p4').fadeOut(500);
         $('.p5').delay(1000).fadeIn(500).delay(5000).fadeOut(500);
+        $('#message').delay(8000).addClass('bye-bye');
+        $('#end').delay(8000).removeClass('hello').addClass('present');
     });
 
 
